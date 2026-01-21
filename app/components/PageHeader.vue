4c0aab34f983a23160b037b65f4673695fe99b46
Hakon Bjarnason <hakonbjarnason@sijthoffmedia.nl>
2026-01-21 21:56:32 +0100

    Set up basic things
    Header, footer, one dynamic route etc.

---
+++ app/components/PageHeader.vue
@@ -0,0 +1,5 @@
+<template>
+  <header>
+    <h1>Page Header</h1>
+  </header>
+</template>
---
+++ app/pages/[artist].vue
@@ -0,0 +1,10 @@
+<template>
+  <div>
+    <h1>Artist</h1>
+    {{  $route.params.artist }}
+  </div>
+</template>
+
+<script setup lang="ts">
+// respond with 404 if slug is not valid
+</script>
